784fbc312ade6b964cb60b7616df6c65449c7754
Omprakash Maurya <omprakash.maurya1604@gmail.com>
2025-07-12 12:46:12 +0530

    Frontend Setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,26 @@
-import Button from "@mui/material/Button";
+import "./index.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage.jsx";
+import SignupPage from "./Pages/SignupPage.jsx";
+import LoginPage from "./Pages/LoginPage.jsx";
+import OnBoardingPage from "./Pages/OnBoardingPage.jsx";
+import NotificationPage from "./Pages/NotificationPage.jsx";
+import CallPage from "./Pages/CallPage.jsx";
+import ChatPage from "./Pages/ChatPage.jsx";
 
 const App = () => {
   return (
-    <Button variant="outlined" color="error">
-      Error
-    </Button>
+    <div className="h-screen">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/onBoarding" element={<OnBoardingPage />} />
+        <Route path="/notification" element={<NotificationPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+    </div>
   );
 };
 
